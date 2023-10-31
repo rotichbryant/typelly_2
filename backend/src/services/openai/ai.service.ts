03ce20e616b6cfab350aa0e3bd0d8690121c1ba6
Bryant Rotich <lepskibryantr@gmail.com>
2023-10-31 11:58:55 +0300

    Fix: ai services

--- backend/src/services/openai/ai.service.ts
+++ backend/src/services/openai/ai.service.ts
@@ -22,39 +22,9 @@ import { RetrievalQAChain } from "langchain/chains";
 @Injectable()
 export class OpenAIService{
 
-    private aiconfig;
-
-    private openai;
-
-    private langChainAi;
-
     constructor(
         private configService: ConfigService,
-        private appModel: AppModel
-    ){
-        this.aiconfig = new Configuration();
-
-        this.langChainAi = new OpenAI({
-            temperature: 0,
-        });
-
-        this.openai = new OpenAIapi(this.aiconfig);
-    }
-
-    getCompletion(){
-        return this.openai.chat.completions.create({
-            messages: [{ role: 'user', content: 'Who am I ?' }],
-            model: 'gpt-3.5-turbo',
-          });
-    }
-
-    // async createCompletion(prompt,) {
-    //     return await this.openai.chat.completions.create({
-    //         model: 'gpt-3.5-turbo',
-    //         messages: [{ role: 'user', content: prompt }],
-    //         stream: true,
-    //     });    
-    // }
+    ){}
 
     async search({ content, chatbot: { app }}){
         try {
@@ -64,7 +34,6 @@ export class OpenAIService{
                 temperature: 0,
                 openAIApiKey: app.api_key, // In Node.js defaults to process.env.OPENAI_API_KEY
             }),
-            // const model   = new ChatOpenAI({ streaming: false, modelName: app.model, temperature: 0, openAIApiKey: app.api_key,}),
             vectorStore   = await ChromaHelper.fromExistingCollection(
                 new OpenAIEmbeddings(),
                 { 
@@ -72,23 +41,13 @@ export class OpenAIService{
                     url: this.configService.get('VECTOR_DB_URL'), 
                 }
             ),
-            prompt        = PromptTemplate.fromTemplate(
-                `Answer the question based only on the following context:{context}, 
-                Question: {question}`
-            ),
-            serializeDocs = (docs: Document[]) =>docs.map((doc) => doc.pageContent).join("\n"),
+            // prompt        = PromptTemplate.fromTemplate(
+            //     `Answer the question based only on the following context:{context}, 
+            //     Question: {question}`
+            // ),
+            // serializeDocs = (docs: Document[]) =>docs.map((doc) => doc.pageContent).join("\n"),
             // Create a chain that uses the OpenAI LLM and HNSWLib vector store.
             chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
-            // chain         = RunnableSequence.from([
-            //     {
-            //         context: retriever.pipe(serializeDocs),
-            //         question: new RunnablePassthrough(),
-            //     },
-            //     prompt,
-            //     model,
-            //     new StringOutputParser(),
-            // ]);
-
             return await chain.stream({
                 query: content
             });      
@@ -98,16 +57,8 @@ export class OpenAIService{
     }
 
     async models(){
-        return await this.openai.models.list();
+        const openai = new OpenAIapi({apiKey:""});
+        return await openai.models.list();
     }
 
-    async engines(){
-        console.log(this.openai);
-        return await this.openai.listEngines();
-    }
-
-    async chatModels(){
-        console.log(this.openai);
-        return await this.openai.Model.list();
-    }
 } 
